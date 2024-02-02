5467b7f07cccbe9a060652432e24057e532a00db
Ashutosh Singh <itsmemyself710@gmail.com>
2024-02-02 22:49:33 +0530

    update

--- StockProject/baseApp/models.py
+++ StockProject/baseApp/models.py
@@ -14,9 +14,9 @@ class IndustrySector(models.Model):
 
 class Stock(models.Model):
     industry = models.ForeignKey(IndustrySector, on_delete=models.CASCADE)
-    stock_name = models.CharField(unique=True ,max_length=150)
-    bse_code = models.CharField(unique=True ,max_length=150)
-    nse_code = models.CharField(unique=True ,max_length=150)
+    stock_name = models.CharField(unique=True, max_length=150)
+    bse_code = models.CharField(unique=True, max_length=150)
+    nse_code = models.CharField(unique=True, max_length=150)
 
 
 class StockData(models.Model):
@@ -32,11 +32,12 @@ class DisplayStock(models.Model):
     current_price = models.FloatField()
 
     def profit(self):
-        stockpriceobj=DisplayStock.objects.filter(name=self.name).order_by('date').first()
+        stockpriceobj = DisplayStock.objects.filter(name=self.name,date__lt=self.date).order_by('-date').first()
+      
         if stockpriceobj:
-            pre_price=stockpriceobj.current_price
-            current_price=self.current_price
-
-            price=round(((current_price-pre_price)/pre_price)*100,1)
+            pre_price = stockpriceobj.current_price
+            current_price = self.current_price
+        
+            price = round(((current_price-pre_price)/pre_price)*100, 1)
             return price
         return None
